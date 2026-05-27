3e581d30d9976bd97d9f6d5b0713e90481fb7581
VinyyDev <vinyciuselias1@gmail.com>
2026-05-26 21:58:38 -0300

    fix: ja to perdido

--- src/test/java/br/com/faculdadedonaduzzi/lab/TaskControllerTest.java
+++ src/test/java/br/com/faculdadedonaduzzi/lab/TaskControllerTest.java
@@ -1,23 +1,26 @@
 package br.com.faculdadedonaduzzi.lab;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class TaskControllerTest {
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+    @LocalServerPort
+    private int port;
 
     @Test
     public void testGetAllTasks() throws Exception {
         // O ERRO INTENCIONAL ESTÁ AQUI: Chamando "/tarefa" em vez de "/tarefas"
-        ResponseEntity<String> response = restTemplate.getForEntity("/tarefa", String.class);
-        assertThat(response.getStatusCode().value()).isEqualTo(200);
+        URL url = new URL("http://localhost:" + port + "/tarefa");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        int status = con.getResponseCode();
+        assertThat(status).isEqualTo(200);
     }
 }
